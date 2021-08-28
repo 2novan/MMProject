ce7fdd6bc73b132d9e3f7392a03ea164cbcc3f86
Denovan Niget <niget.denovan@gmail.com>
2021-08-29 00:19:04 +0200

    routes conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  authenticated :user do
+    root to: 'pages#home'
+  end
   unauthenticated :user do
     root to: 'pages#welcome'
   end
-  authenticated :user do
-    root to: redirect("/home"), as: :authenticated_root
-  end
+
+  get '/components' => 'pages#components', as: :components
 
   resources :game, only: [:edit, :create]
   resources :player, only: [:create, :edit, :show ]
