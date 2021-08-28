d43688165a18875cc4cfdb90f64c1f5e234dcc75
Denovan Niget <niget.denovan@gmail.com>
2021-08-28 23:40:58 +0200

    authentication + login + landing OK

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/components' => 'pages#components'
   resources :game, only: [:edit, :create]
-  resources :player, only: [:create, :edit, :show ]
-
+  resources :user, only: [:edit, :show]
 end
--- db/migrate/20210827162705_add_players_to_users.rb
+++ db/migrate/20210827162705_add_players_to_users.rb
@@ -2,5 +2,7 @@ class AddPlayersToUsers < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :username, :string, index: true, unique: true
     add_column :users, :total_wins, :integer, default: 0
+    
+
   end
 end
